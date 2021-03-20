90f2cdd669b5564da47ccf423b45e94f1cdec60a
Albi.Goxhaj <Albi.Goxhaj@fti.edu.al>
2021-03-20 17:06:42 +0100

    first commit

---
+++ Zoom Integration/Models/RequestDTO/Settings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zoom_Integration.Models.RequestDTO
+{
+    public class Settings
+    {
+    }
+}
