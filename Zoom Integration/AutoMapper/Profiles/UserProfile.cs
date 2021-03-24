cf9bde581b0a61e744015197e1656253e8379f4b
Albi.Goxhaj <Albi.Goxhaj@fti.edu.al>
2021-03-24 23:06:29 +0100

    first commit

---
+++ Zoom Integration/AutoMapper/Profiles/UserProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Zoom_Integration.Models.ResponseDTO;
+using Zoom_Integration.ViewModels;
+
+namespace Zoom_Integration.AutoMapper.Profiles
+{
+    public class UserProfile : Profile
+    {
+
+        public UserProfile()
+        {
+            CreateMap<User, UserViewModel>()
+                   .ForMember(dest =>
+                       dest.first_name,
+                       opt => opt.MapFrom(src => src.first_name))
+                   .ForMember(dest =>
+                       dest.last_name,
+                       opt => opt.MapFrom(src => src.last_name))
+                   .ForMember(dest =>
+                       dest.email,
+                       opt => opt.MapFrom(src => src.email));
+        }
+    }
+}
---
+++ Zoom Integration/ViewModels/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zoom_Integration.ViewModels
+{
+    public class UserViewModel
+    {
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string email { get; set; }
+    }
+}
