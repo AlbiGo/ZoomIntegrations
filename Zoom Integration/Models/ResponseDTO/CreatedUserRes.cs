d7eb956bf5a672b96124e7c301a0a81a68812213
Albi.Goxhaj <Albi.Goxhaj@fti.edu.al>
2021-03-30 17:17:17 +0200

    first commit

---
+++ Zoom Integration/Models/ResponseDTO/CreatedUserRes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zoom_Integration.Models.ResponseDTO
+{
+    public class CreatedUserRes
+    {
+        public string id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string email { get; set; }
+        public string type { get; set; }
+    }
+}
