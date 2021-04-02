785cc5797bb5300908c49fd3d7a058f7b00e3229
Albi.Goxhaj <Albi.Goxhaj@fti.edu.al>
2021-04-02 15:52:56 +0200

    first commit

---
+++ Zoom Integration/Models/ResponseDTO/MeetingrPageInfoDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Zoom_Integration.Models.Entities;
+using Zoom_Integration.ViewModels;
+
+namespace Zoom_Integration.Models.ResponseDTO
+{
+    public class MeetingrPageInfoDTO
+    {
+        public int page_count { get; set; }
+        public int page_number { get; set; }
+        public int page_size { get; set; }
+        public int total_records { get; set; }
+        public List<MeetingViewModel> meetings { get; set; }
+    }
+}
---
+++ Zoom Integration/Models/ResponseDTO/UserPageInfoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zoom_Integration.Models.ResponseDTO
+{
+    public class UserPageInfoDTO
+    {
+        public int page_count { get; set; }
+        public int page_number { get; set; }
+        public int page_size { get; set; }
+        public int total_records { get; set; }
+        public List<User> users { get; set; }
+    }
+}
