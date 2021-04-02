116debc326f05169d9c98f94e13e34c84dc3beaf
Albi.Goxhaj <Albi.Goxhaj@fti.edu.al>
2021-04-02 15:56:56 +0200

    renameFiles

---
+++ Zoom Integration/Models/ResponseDTO/ApiResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zoom_Integration.Models.ResponseDTO
+{
+    public class ApiResponseDTO
+    {
+        public int code { get; set; }
+        public string message { get; set; }
+    }
+}
