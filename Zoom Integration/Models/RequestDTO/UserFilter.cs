4be59c9cbd7769a65c48f4bc25324f2f05315f7c
Albi.Goxhaj <Albi.Goxhaj@fti.edu.al>
2021-04-10 14:37:20 +0200

    commit

---
+++ Zoom Integration/Models/RequestDTO/UserFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zoom_Integration.Models.RequestDTO
+{
+    public class UserFilter
+    {
+        public string status { get; set; }
+        public string role_id { get; set; }
+        public string include_fields { get; set; }
+        public int? page_number { get; set; }
+        public int? page_size { get; set; }
+        public string next_page_token { get; set; }
+    }
+}
---
+++ Zoom Integration/Utilites/Utilities.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Zoom_Integration.Utilites
+{
+    public static class Utilities
+    {
+        public static string buildQueryString(string filter)
+        {
+            JObject jObj = (JObject)JToken.FromObject(filter);
+
+            //Build query string
+            var query = String.Join("&",
+                            jObj.Children().Cast<JProperty>()
+                            .Select(
+                                jp => (!string.IsNullOrEmpty(jp.Value.ToString()) ? (jp.Name + "=") : string.Empty) + (!string.IsNullOrEmpty(jp.Value.ToString()) ? HttpUtility.UrlEncode(jp.Value.ToString()) : string.Empty)
+                            ).Where(p => !string.IsNullOrEmpty(p.ToString()))
+                            );
+            return query;
+        }
+    }
+}
